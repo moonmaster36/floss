d796414128f00b004b42721599c76283bfb90c87
moonmaster36 <76980875+moonmaster36@users.noreply.github.com>
2022-05-22 23:05:53 -0500

    Swapped out old single-loop floss with working microservice floss. CLI may need to be updated

--- floss.py
+++ floss.py
@@ -1,3 +1,9 @@
+"""
+Frank Vaughan
+CS361: Software Engineering I
+Assignment 7
+"""
+
 import time
 import os
 import pathlib
@@ -5,29 +11,32 @@ import sys
 import argparse
 import json
 import csv
+import cmd
+
 
 class Floss:
     def __init__(self):
         self.speed = None
         self.stream_path = None
+        self.input_path = None
         self.output_path = None
 
     def argument_handler(self):
-        """Returns arguments given from command line.
-
-        Returns:
-            stream_path: path to stream data-paths from
-            output_path: path to write data to
+        """
+        * Returns arguments given from command line.
+        * Arguments:
+        *   stream_path: path to stream data-paths from.
+        *   json_file: path to write data to.
+        * Both arguments are required.
+        * Allows users to get help by adding modifier -help
         """
         parser = argparse.ArgumentParser(prog="floss",
-                                         description="Converts stock data from text and csv files to json",
-                                         usage="py %(prog)s.py [options] input_path output_path")
+                                         description="Converts text and csv files to json",
+                                         usage="%(prog)s [options] input_path output_path")
         parser.add_argument("-s", "--speed", help="Changes how quickly the stream_path is re-checked.",
-                            type=float, action='store', default=1)
-        parser.add_argument(
-            "input_path", help="Text file containing path to data", type=str)
-        parser.add_argument(
-            "output_path", help="JSON file to write data to", type=str)
+                            type=int, action='store', default=1)
+        parser.add_argument("input_path", help="Text file containing path to data file", type=str)
+        parser.add_argument("output_path", help="JSON file to write data to", type=str)
 
         args = parser.parse_args()
 
@@ -35,25 +44,13 @@ class Floss:
             print(f'speed cannot be negative')
 
         self.speed = args.speed
-        print(f'speed = {self.speed}')
-
         stream_path = args.input_path
         output_path = args.output_path
-        self.stream_path = output_path
-        self.output_path = output_path
 
         return stream_path, output_path
 
-    @staticmethod
-    def get_txt_data(txt_file):
-        """Fetches data from .txt file
-
-        Args:
-            txt_file (.txt): file containing data
-
-        Returns:
-            list: list of dictionaries
-        """
+    def get_txt_data(self, txt_file):
+        """Handles retrieval of data from .txt files."""
         if not os.path.isfile(txt_file):
             print('File does not exist.')
             return
@@ -78,15 +75,18 @@ class Floss:
                 cur['ticker'] = ticker
 
                 stocks.append(cur)
+
             return {'data': stocks}
 
-    @staticmethod
-    def txt_service(data, output_path):
-        """Converts data from .txt source to json then writes data to output_path.
+    def txt_service(self, data, output_path):
+        """
+        * Handles .txt input files.
+        * Retrieves data from file specified in txt_file, converts data to JSON,
+            then writes data to file specified in json_file.
 
-        Args:
-            data (list): stock data
-            output_path (.json file): path to output file
+        :param data_path: source of stock data
+        :param output_path: file to write json data to
+        :return: None
         """
         with open(output_path, 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
@@ -97,18 +97,8 @@ class Floss:
 
             print(f'floss wrote data to {file_path}')
 
-    @staticmethod
-    def get_csv_data(path):
-        """Fetches data from .csv file and converts the data
-            into a list .
-
-        Args:
-            path (.csv): path to csv data
-
-        Returns:
-            list of dictionaries, each dictionary is a row from csv data
-        """
-        with open(path, 'r') as file:
+    def get_csv_data(self, path):
+        with open(path, 'r', encoding='utf-8-sig') as file:
             reader = csv.DictReader(file)
             data = []
             for row in reader:
@@ -116,32 +106,23 @@ class Floss:
             return data
 
     def csv_service(self, csv_path, output_path):
-        """Extracts data from .csv file and writes it to the
-            specified output file in JSON format.
-
-        Args:
-            csv_path (.csv): source of data
-            output_path (.json): file to write data to
+        """
+        * Extracts data from .csv file and writes it to the
+            specified output in JSON format.
+        * csvfile: path to .cvs file containing data.
+        * json_file: path to .json file to write data.
         """
         data = self.get_csv_data(csv_path)
+
         with open(output_path, 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
 
             # Get last part of path for cleaner printing.
+            # cur_path = os.path.basename(os.path.normpath(txt_file))
             file_path = pathlib.PurePath(output_path)
-            print(f'floss wrote data from {csv_path} to {file_path}')
+            print(f'floss wrote data to {file_path}')
 
-    @staticmethod
-    def validate_arguments(stream_path, output_path):
-        """Validates file paths given as arguments.
-
-        Args:
-            stream_path (.txt): file to read data paths from
-            output_path (.json): file to write data to
-
-        Returns:
-            bool: True if paths are valid, false if not
-        """        
+    def validate_arguments(self, stream_path, output_path):
         # Check if stream path exists
         if not os.path.isfile(stream_path):
             print('Stream file not found.')
@@ -157,28 +138,18 @@ class Floss:
 
         # Validate stream/output file types.
         if stream_type != '.txt':
-            print(
-                f'Invalid stream file type {stream_type}, streams must be .txt files.')
+            print(f'Invalid stream file type {stream_type}, streams must be .txt files.')
             return False
 
         if output_type != '.json':
-            print(
-                f'Invalid output file type {output_type}, output must be a .json file.')
+            print(f'Invalid output file type {output_type}, output must be a .json file.')
             return False
 
         return True
 
     def file_service(self, input_path, output_path):
-        """Reads paths from stream file, then converts
-            the .txt or .csv file into json data.
+        # input_file_size = os.path.getsize(input_path)
 
-        Args:
-            input_path (.txt or .csv): path to data
-            output_path (.json): path to output file
-
-        Returns:
-            _type_: True if data was written, False if not.
-        """
         # Check if input path exists
         if not os.path.isfile(input_path):
             print('Input file not found.')
@@ -217,51 +188,49 @@ class Floss:
 
         return True
 
-    def read_file_stream(self, stream_path):
-        """Core loop of program.
-            Reads data path files, fetches data, writes data to output.
-
-        Args:
-            stream_path (.txt): input file to read data paths from
-        """
+    def read_file_stream(self, stream_path, output_path):
+        """Main loop of program."""
         # Wait for changes in input file.
         last_size = 0
-        last_path = ''
         run = True
-
         while run:
-            # On change, copy contents of input file. Write contents to given output JSON file.
-            file_size = os.path.getsize(stream_path)
-            output_size = os.path.getsize(self.output_path)
-
-            if file_size > 0 and output_size == 0:
-                # Attempt to read the path input from stream file
-                with open(stream_path, "r") as f:
-                    data_path = f.readline()
-                    if data_path != last_path:  # If this is a new path, fetch data.
-                        # Track the last stock_path entered
-                        last_size = file_size
-                        last_path = data_path
-                        result = self.file_service(
-                            data_path, self.output_path)
-                        run = result
-
+            # Attempt to read the path input from stream file
+            with open(stream_path, "r") as f:
+                data_path = f.readline()
             time.sleep(self.speed)
 
+            # On change, copy contents of input file. Write contents to given output JSON file.
+            file_size = os.path.getsize(stream_path)
+            output_size = os.path.getsize(output_path)
+            if file_size == 0:
+                print(f'Waiting for input!')
+            elif file_size != last_size and output_size == 0 and len(data_path) > 0:
+                print(f'data_path: {data_path}')
+                # Track the last stock_path entered
+                last_size = file_size
+
+                result = self.file_service(data_path, output_path)
+                run = result
+            elif output_size == 0 and len(data_path) > 0:
+                # Output file is empty, write data to output
+                last_size = file_size
+
+                result = self.file_service(data_path, output_path)
+                run = result
+            elif file_size != last_size and output_size > 0:
+                print('Waiting for space in output.')
+            # else:
+                # print('Waiting for space in output.')
         print(f'\nStream stopped.')
 
-    def main_loop(self):       
-        """Calls all functions needed to operate program.
-
-        Returns:
-            int: 0 if program exited successfuly, else 1
-        """
-        
+    def main_loop(self):
         # get args
         stream_path, output_path = self.argument_handler()
         # stream_path = 'stream.txt'
         # output_path = 'output.json'
-        print(f'Reading data file paths from {stream_path}')
+        print(F'stream_path: {stream_path}')
+        print(F'output_path: {output_path}\n')
+
         # Validate args.
         if not self.validate_arguments(stream_path, output_path):
             return 'Invalid args'
@@ -283,9 +252,15 @@ class Floss:
                 sys.exit(1)
 
         # Read from stream file for input paths.
-        self.read_file_stream(stream_path)
-        sys.exit(0)
+        stream = self.read_file_stream(stream_path, output_path)
+
+
+class Controller:
+    def run_program(self):
+        floss = Floss()
+        floss.main_loop()
+
 
 if __name__ == '__main__':
-    floss = Floss()
-    floss.main_loop()
+    controller = Controller()
+    controller.run_program()
